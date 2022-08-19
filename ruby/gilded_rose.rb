80f3a04dfb874767301ed1d450f5d6a598b89026
elioflo <eliofloreschoque2357@gmail.com>
2022-08-19 20:57:52 -0300

    Star to refactorate update_quality...
    co-authored-by: felipelopezoa <lopezortegaaceves@gmail.com>
    co-authored-by: willcab <willianeduardocg13@gmail.com>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,18 +4,37 @@ class GildedRose
     @items = items
   end
 
+  def normal_item(item)
+    item.sell_in -= 1 
+    if check_quality(item.quality)
+      item.quality -= reduce_quality(item.sell_in)
+    end
+  end
+
+  def reduce_quality(days)
+    days <= 0 ? 2 : 1
+  end
+
+  def check_quality(quality)
+    quality >= 0
+  end
+
+  def compare_sell_in(days)
+    return 1 if days.between?(6, 10)
+    return 2 if days < 6
+  end
+
   def update_quality()
+    special = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"]
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if !special.include? item.name
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          item.quality = item.quality - 1
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.name == special[1]
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -35,10 +54,8 @@ class GildedRose
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+            if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros"
+              item.quality = item.quality - 1
             end
           else
             item.quality = item.quality - item.quality
