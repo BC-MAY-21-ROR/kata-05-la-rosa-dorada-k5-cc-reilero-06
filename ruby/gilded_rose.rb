da1d64ef663137bc85075c3ec91c722bc4d7b96a
elioflo <eliofloreschoque2357@gmail.com>
2022-08-22 20:48:26 -0300

    Refactor: check_quality method
    co-authored-by: felipelopezoa <lopezortegaaceves@gmail.com>
    co-authored-by: willcab <willianeduardocg13@gmail.com>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,14 +6,14 @@ class GildedRose
 
   def normal_item(item) #method that applies the rules to normal items
     item.sell_in -= 1 
-    if check_quality(item.quality)
+    if normal_check_quality(item.quality)
       item.quality -= reduce_quality(item.sell_in)
     end
   end
 
   def conjured_item(item) #method that appplies the rules to conjured items
     item.sell_in -= 1 #if item.sell_in > 0
-    if check_quality(item.quality)
+    if special_check_quality(item.quality)
       item.quality -= reduce_quality(item.sell_in)*2
     end
   end
@@ -23,9 +23,12 @@ class GildedRose
     days <= 0 ? 2 : 1
   end
 
-  def check_quality(quality) #method that check that the quality is more than 1 
-    quality.between?(0,49)
+  def normal_check_quality(quality) #method that check that the quality is more than 1 
+    quality > 0 and quality < 50
+  end
 
+  def special_check_quality(quality) #method that check that the quality is more than 1 
+    quality >= 0 and quality < 50
   end
 
   def compare_sell_in(days, quality) #method that belongs to items Backstage
@@ -37,14 +40,14 @@ class GildedRose
 
   def aged_item(item) #method that belongs to items Aged Brie
     item.sell_in -= 1
-    if check_quality(item.quality)
+    if special_check_quality(item.quality)
       item.quality += reduce_quality(item.sell_in)
     end
   end 
 
   def backstage_item(item) #method that belongs to items Backstage
     item.sell_in -= 1
-    if check_quality(item.quality)
+    if special_check_quality(item.quality)
       item.quality = compare_sell_in(item.sell_in, item.quality)
     end
   end
@@ -63,50 +66,7 @@ class GildedRose
       else
         normal_item(item)
       end
-=begin
-      if !special.include? item.name
-        if item.quality > 0
-          item.quality = item.quality - 1
-        end
-      else
-        if item.quality < 50 && item.name !=special[3]
-          item.quality = item.quality + 1
-          if item.name == special[1]
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros" && item.name != "Conjured Mana Cake"
-              item.quality = item.quality - 1
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-      conjured_item(item) if item.name == special[3]
     end
-=end
-  end
   end
 end
 
