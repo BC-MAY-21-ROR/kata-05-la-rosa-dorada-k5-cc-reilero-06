44460124bdbe8a842191c8016562abb86b812ac9
willcab <willianeduardocg13@gmail.com>
2022-08-22 16:18:07 -0500

    refact : if statements replace
     - replacement of if statements by case statement
     - add method for conjured items
    
    Co-authored-by: felipelopezoa <lopezortegaaceves@gmail.com>
    Co-authored-by: elioflo <eliofloreschoque2357@gmail.com>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -10,13 +10,18 @@ class GildedRose
       item.quality -= reduce_quality(item.sell_in)
     end
   end
-
+  def conjured_item(item)
+    item.sell_in -= 1 #if item.sell_in > 0
+    if check_quality(item.quality)
+      item.quality -= reduce_quality(item.sell_in)*2
+    end
+  end
   def reduce_quality(days)
     days <= 0 ? 2 : 1
   end
 
   def check_quality(quality)
-    quality >= 0
+    quality > 0
   end
 
   def compare_sell_in(days)
@@ -25,14 +30,26 @@ class GildedRose
   end
 
   def update_quality()
-    special = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"]
+    special = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros","Conjured Mana Cake"]
     @items.each do |item|
+      case item.name
+      when /Aged/
+        #aged_item(item)
+      when /Backstage/
+        #backstage_item(item)
+      when /Sulfuras/
+      when /Conjured/
+        conjured_item(item)
+      else
+        normal_item(item)
+      end
+=begin
       if !special.include? item.name
         if item.quality > 0
           item.quality = item.quality - 1
         end
       else
-        if item.quality < 50
+        if item.quality < 50 && item.name !=special[3]
           item.quality = item.quality + 1
           if item.name == special[1]
             if item.sell_in < 11
@@ -54,7 +71,7 @@ class GildedRose
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros"
+            if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros" && item.name != "Conjured Mana Cake"
               item.quality = item.quality - 1
             end
           else
@@ -66,7 +83,10 @@ class GildedRose
           end
         end
       end
+      conjured_item(item) if item.name == special[3]
     end
+=end
+  end
   end
 end
 
