d193715bebebf888b48b4416c47fc4dc62e46f2b
willcab <willianeduardocg13@gmail.com>
2022-08-23 16:15:08 -0500

    Add: report rubycritic  -folder app  -folder test Co-authored-by: felipelopezoa <lopezortegaaceves@gmail.com> Co-authored-by: elioflo <eliofloreschoque2357@gmail.com>

--- ruby/test/gilded_rose_spec.rb
+++ ruby/test/gilded_rose_spec.rb
@@ -1,19 +1,17 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require_relative '../app/gilded_rose'
 
 describe GildedRose do
 
   describe "#update_quality" do
-    it '"Sulfuras", being a legendary item, never has to be sold or decreases in Quality' do
+    it '"Sulfuras", being a legendary item, never has to be sold or decreases in Quality' do# HighComplexity
       items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=80)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "Sulfuras, Hand of Ragnaros"
       expect(items[0].sell_in).to eq 10
       expect(items[0].quality).to eq 80
     end
-
-    it '"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-    Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-    Quality drops to 0 after the concert' do
+ 
+    it '"Backstage passes", increment quality on sellin left but decrese to 0 when sell_in is 0 ' do # HighComplexity
       items = [
         Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
         Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20),
@@ -27,7 +25,7 @@ describe GildedRose do
       expect(items[3].quality).to eq 0
     end
 
-    it "Aged Brie actually increases in Quality the older it gets" do
+    it "Aged Brie actually increases in Quality the older it gets" do # HighComplexity
       items = [
         Item.new(name="Aged Brie", sell_in=2, quality=0),
         Item.new(name="Aged Brie", sell_in=0, quality=0)
@@ -55,7 +53,7 @@ describe GildedRose do
       expect(items[0].quality).to eq 0
     end
 
-    it '"Conjured" items degrade in Quality twice as fast as normal items' do
+    it '"Conjured" items degrade in Quality twice as fast as normal items' do # HighComplexity
       items = [Item.new(name="Conjured Mana Cake", sell_in=3, quality=6)]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 4
