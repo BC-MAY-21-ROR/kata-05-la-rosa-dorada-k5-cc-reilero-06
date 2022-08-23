e80af154386be128a7f9d67ed23da2ac7fe25cd4
willcab <willianeduardocg13@gmail.com>
2022-08-23 17:01:19 -0500

    refact test: - item sulfuras test - item Backstage test
    Co-authored-by: felipelopezoa <lopezortegaaceves@gmail.com>
    Co-authored-by: elioflo  <eliofloreschoque2357@gmail.com>

--- ruby/test/gilded_rose_spec.rb
+++ ruby/test/gilded_rose_spec.rb
@@ -4,25 +4,22 @@ describe GildedRose do
 
   describe "#update_quality" do
     it '"Sulfuras", being a legendary item, never has to be sold or decreases in Quality' do# HighComplexity
+      expect_value = [10,80]
       items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=80)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "Sulfuras, Hand of Ragnaros"
-      expect(items[0].sell_in).to eq 10
-      expect(items[0].quality).to eq 80
+      return_value = [items[0].sell_in, items[0].quality]
+      expect(return_value).to eq expect_value
     end
  
     it '"Backstage passes", increment quality on sellin left but decrese to 0 when sell_in is 0 ' do # HighComplexity
-      items = [
-        Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-        Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20),
-        Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=20),
-        Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=20)
-      ]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 21
-      expect(items[1].quality).to eq 22
-      expect(items[2].quality).to eq 23
-      expect(items[3].quality).to eq 0
+      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20)]
+      days=[15,10,5,0]
+      quality_return=days.map do |day|
+        items[0].sell_in=day
+        GildedRose.new(items).update_quality()
+        items[0].quality
+      end
+      expect(quality_return).to eq [21,23,26,0]
     end
 
     it "Aged Brie actually increases in Quality the older it gets" do # HighComplexity
